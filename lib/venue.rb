2e515871c5e8119437c89bf185406073d9619e76
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-03-24 16:58:46 -0400

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,31 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(new_patron)
+    @patrons << new_patron
+  end
+
+  def yell_at_patrons
+    yelled_at_patrons = []
+    @patrons.each do |patron|
+      yelled_at_patrons << patron.upcase
+    end
+    return yelled_at_patrons
+  end
+
+  def over_capacity
+    if @patrons.length <= 4
+      return false
+    else
+      return true 
+    end
+  end
+
 end
